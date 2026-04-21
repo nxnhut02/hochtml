00436896b6bce5c766a06c0ef7b048a6a8c41d94
nxnhut02 <nhut0272@gmail.com>
2026-04-21 22:25:17 +0700

    calculator

---
+++ calculator.js
@@ -0,0 +1,78 @@
+let currentOperand = '0';
+let previousOperand = '';
+let operation = undefined;
+
+const currentTextElement = document.getElementById('current-operand');
+const previousTextElement = document.getElementById('previous-operand');
+
+function appendNumber(number) {
+    if (number === '.' && currentOperand.includes('.')) return;
+    if (currentOperand === '0' && number !== '.') {
+        currentOperand = number.toString();
+    } else {
+        currentOperand = currentOperand.toString() + number.toString();
+    }
+    updateDisplay();
+}
+
+function chooseOperation(op) {
+    if (currentOperand === '') return;
+    if (previousOperand !== '') {
+        compute();
+    }
+    operation = op;
+    previousOperand = currentOperand;
+    currentOperand = '';
+    updateDisplay();
+}
+
+function compute() {
+    let computation;
+    const prev = parseFloat(previousOperand);
+    const current = parseFloat(currentOperand);
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (operation) {
+        case '+':
+            computation = prev + current;
+            break;
+        case '-':
+            computation = prev - current;
+            break;
+        case '*':
+            computation = prev * current;
+            break;
+        case '/':
+            computation = current === 0 ? "Error" : prev / current;
+            break;
+        default:
+            return;
+    }
+    currentOperand = computation;
+    operation = undefined;
+    previousOperand = '';
+    updateDisplay();
+}
+
+function clearDisplay() {
+    currentOperand = '0';
+    previousOperand = '';
+    operation = undefined;
+    updateDisplay();
+}
+
+function deleteNumber() {
+    if (currentOperand === '0') return;
+    currentOperand = currentOperand.toString().slice(0, -1);
+    if (currentOperand === '') currentOperand = '0';
+    updateDisplay();
+}
+
+function updateDisplay() {
+    currentTextElement.innerText = currentOperand;
+    if (operation != null) {
+        previousTextElement.innerText = `${previousOperand} ${operation}`;
+    } else {
+        previousTextElement.innerText = '';
+    }
+}
